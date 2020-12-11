47cb361c91df1c1852787ae7a09fa7d9869efa0a
Daniel A. Rodriguez <drodrigu.ln@gmail.com>
2020-12-11 09:31:45 -0700

    build: add lighthouse github action

---
+++ lighthouserc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    ci: {
+        collect: {
+            url: ['http://localhost:3000/'],
+            startServerCommand: 'rails server -e production',
+        },
+    },
+};
