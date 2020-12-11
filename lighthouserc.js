4a0472e693f60cd8dbec49c8837d2aca63c80696
Daniel A. Rodriguez <drodrigu.ln@gmail.com>
2020-12-11 10:41:22 -0700

    fix: lighthouse github action

--- lighthouserc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    ci: {
-        collect: {
-            url: ['http://localhost:3000/'],
-            startServerCommand: 'rails server -e production',
-        },
-    },
-};
