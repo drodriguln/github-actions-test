a7383595c1f8ecc4538ddb3061a0710d53fd216c
Daniel A. Rodriguez <drodrigu.ln@gmail.com>
2020-12-11 11:17:05 -0700

    build: gh action lighthouse cleanup

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,16 +1,21 @@
 module.exports = {
     ci: {
         assert: {
-            assertions: {
-                'categories:performance': [
-                    'warn',
-                    {'minScore': 0.9},
-                ],
-                'categories:accessibility': [
-                    'error',
-                    {'minScore': 1},
-                ],
-            }
-        }
-    }
+          assertions: {
+              'categories:performance': [
+                  'warn',
+                  { 'minScore': 0.9 },
+              ],
+              'categories:accessibility': [
+                  'error',
+                  { 'minScore': 1 },
+              ],
+          }
+        },
+        collect: {
+            url: 'http://localhost:3000/',
+            startServerCommand: 'npm run start',
+            uploadArtifacts: true,
+        },
+    },
 };
