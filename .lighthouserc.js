fc45af9c8e8fd30937509214dc5ccce2401b5ab5
Daniel A. Rodriguez <drodriguln@icloud.com>
2021-01-20 08:36:00 -0700

    feat: add prettier gh action (#7)
    * feat: add prettier gh action
    
    * build: remove unused lighthouse config
    
    * fix: only run prettier on src directory
    
    * fix: only run prettier on src directory
    
    * chore: test prettier with trivial change
    
    * fix: re-add lighthouse gh action and config
    
    Co-authored-by: Daniel A. Rodriguez <drodrigu.ln@gmail.com>

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,16 +1,16 @@
 module.exports = {
     ci: {
         assert: {
-          assertions: {
-              'categories:performance': [
-                  'error',
-                  { 'minScore': 0.8 },
-              ],
-              'categories:accessibility': [
-                  'error',
-                  { 'minScore': 0.8 },
-              ],
-          }
+            assertions: {
+                'categories:performance': [
+                    'error',
+                    { 'minScore': 0.8 },
+                ],
+                'categories:accessibility': [
+                    'error',
+                    { 'minScore': 0.8 },
+                ],
+            }
         },
         collect: {
             uploadArtifacts: true,
